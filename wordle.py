1ac96b7bb8e7e3ed609aa434113731413ba8f909
ZacWang123 <Zac.W2002@yahoo.com>
2024-12-21 22:04:39 +1000

    grid entry limitation

--- wordle.py
+++ wordle.py
@@ -1,13 +1,31 @@
 from tkinter import *
 
 def create_letterbox(window):
-    entry = Entry(window, width = 1)
-    entry.grid(pady=10)
+    grid = Frame(window)
+    grid['bg'] = "#3b3b3b"
+    grid.pack(padx = (0, 0))
+    grid.pack(pady = (50, 0))
+    
+    vcmd = (window.register(validate_entry), "%P")
+    
+    for x in range(6):
+        for y in range(5):
+            entry = Entry(grid, width = 2, font=("Arial", 36), justify = "center", validate="key", validatecommand = vcmd)
+            entry.grid(row = x, column = y, padx = 10, pady = 10)
+            
 
+def validate_entry(entry):
+    if len(entry) <= 1:
+        return True
+    else:
+        return False
+    
+    
 def create_window():
     window = Tk()
     window.title("Wordle")
-
+    window['bg'] = "#3b3b3b"
+    
     windowWidth = 600
     windowHeight = 800
     
