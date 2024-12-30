c4c86edc0fa5cd81594b0573f73ef04565fa1cae
ZacWang123 <Zac.W2002@yahoo.com>
2024-12-30 19:52:15 +1000

    complete

--- wordle.py
+++ wordle.py
@@ -1,7 +1,10 @@
 from tkinter import *
+import tkinter.messagebox as messagebox
 import random
+import sys
 
 gridEntries = [[None for x in range(5)] for y in range(6)] 
+keyboardEntries = [None for x in range(28)]
 word = ""
 row = 0
 col = 0
@@ -20,12 +23,84 @@ def game_intervels(window):
             col += 1
             if col < 5:
                 enable_entry(row, col)
-        
+    
     window.after(20, game_intervels, window)
+    
+def button_click(letter):
+    global row
+    global col
+    
+    if col < 5:
+        if (gridEntries[row][col].get() == ""):
+            gridEntries[row][col].insert(0, letter)
+            
+    
+def create_buttons(window):
+    global keyboard
+    
+    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    keyboard = Frame(window)
+    keyboard['bg'] = "#222222"
+    keyboard.pack(padx=10, pady=10)    
+    
+    for i, letter in enumerate(alphabet):
+        button = Button(keyboard, 
+                        bd = 0,
+                        text=letter, 
+                        width=5, 
+                        height=2, 
+                        command=lambda l=letter: button_click(l))
+        button.grid(row=i // 7, column=i % 7, padx=5, pady=5)
+        keyboardEntries[i] = button
+        
+    enterButton = Button(keyboard, 
+                        bd = 0,
+                        text="Enter", 
+                        width=5, 
+                        height=2, 
+                        command=lambda l=letter: enter_button(window))
+    enterButton.grid(row = 3, column = 5, padx=5, pady=5)
+    keyboardEntries[26] = enterButton
+    
+    backButton = Button(keyboard, 
+                        bd = 0,
+                        text="<-", 
+                        width=5, 
+                        height=2, 
+                        command=lambda l=letter: backspace_button())
+    backButton.grid(row = 3, column = 6, padx=5, pady=5)
+    keyboardEntries[27] = backButton
+    
+def enter_button(window):
+    global row
+    global col
+
+    if col == 5:
+        if not validate_word():
+            invalid_word_popup(window)
+            return
+        if row < 6:
+            check_row(row, window)
+            row += 1
+            if row == 6:
+                end_game("lose", window)
+            col = 0
+            enable_entry(row, col)
+            
+def backspace_button():
+    global row
+    global col
+    
+    if col > 0:
+        if col < 5:
+            disable_entry(row, col)
+        col -= 1
+        enable_entry(row, col)
+        delete_entry(row, col)
 
 def create_letterbox(window):
     grid = Frame(window)
-    grid['bg'] = "#3b3b3b"
+    grid['bg'] = "#222222"
     grid.pack(padx = (0, 0))
     grid.pack(pady = (50, 0))
     
@@ -33,17 +108,28 @@ def create_letterbox(window):
     
     for x in range(6):
         for y in range(5):
-            entry = Entry(grid, width = 2, font=("Arial", 36), justify = "center", validate="key", validatecommand = vcmd)
-            entry.config(insertbackground = 'white')
+            entry = Entry(grid,
+                        width = 2,
+                        bg = "#222222",
+                        fg = "#FFFFFF",
+                        font=("Arial", 36),
+                        insertwidth = 0,
+                        insertbackground = "#222222",
+                        relief = "solid",
+                        highlightthickness = 5,
+                        highlightbackground = "grey",
+                        justify = "center",
+                        validate="key",
+                        validatecommand = vcmd)            
             entry.grid(row = x, column = y, padx = 10, pady = 10)
             entry.bind("<BackSpace>", lambda event, x=x, y=y: detect_backspace(event, x, y))
             entry.bind("<KeyPress>", lambda event, x=x, y=y: upper_case(event, x, y))
-            entry.bind("<Return>", lambda event, x=x, y=y: detect_enter(event, x, y))
+            entry.bind("<Return>", lambda event, x=x, y=y: detect_enter(event, x, y, window))
             gridEntries[x][y] = entry
             disable_entry(x, y)
             
 def disable_entry(x, y):
-    gridEntries[x][y].config(state='disabled', disabledforeground = "black")
+    gridEntries[x][y].config(state='disabled', disabledbackground = "#222222", disabledforeground = "#FFFFFF")
     
 def enable_entry(x,y):
     gridEntries[x][y].config(state='normal')
@@ -61,13 +147,25 @@ def validate_entry(entry):
 def generate_word():
     global word
     
-    random_number = random.randint(0, 2309)
+    random_number = random.randint(0, 14855)
     
     with open("5-letter_words.txt", "r") as file:
         for lineNum, line in enumerate(file):
             if lineNum == random_number:
                 word = line.strip().upper()
                 break
+            
+def validate_word():
+    userWord = ""
+    
+    for y in range(5):
+        userWord += gridEntries[row][y].get()
+        
+    with open("5-letter_words.txt", "r") as file:
+        for lineNum, line in enumerate(file):
+            if line.strip().upper() == userWord:
+                return True
+    return False
 
 def detect_backspace(event, x, y):
     global row
@@ -80,12 +178,91 @@ def detect_backspace(event, x, y):
         enable_entry(row, col)
         delete_entry(row, col)
         
-def detect_enter(event, x, y):
+def invalid_word_popup(window):
+    popup = Toplevel(window)
+    popup.title("Invalid Word")
+    
+    popup_width = 300
+    popup_height = 100
+    
+    screenWidth = window.winfo_screenwidth()
+    screenHeight = window.winfo_screenheight()
+
+    positionX = int(screenWidth /2 - popup_width/2)
+    positionY = int(screenHeight / 4 - popup_height/2)
+    
+    popup.geometry(f"{popup_width}x{popup_height}+{positionX}+{positionY}")
+    
+    label = Label(popup, text="Invalid word! Please try again.", font=("Arial", 12))
+    label.pack(pady=20)
+    popup.after(500, popup.destroy)
+    
+def detect_enter(event, x, y, window):
     global row
     global col
     
     if col == 5:
-        print("asdasdasdasd")
+        if not validate_word():
+            invalid_word_popup(window)
+            return
+        if row < 6:
+            check_row(row, window)
+            row += 1
+            if row == 6:
+                end_game("lose", window)
+            col = 0
+            enable_entry(row, col)
+            
+def check_row(row, window):
+    global word
+    
+    userLetters = [0] * 5
+    userWord = ""
+    
+    for y in range(5):
+        letter = gridEntries[row][y].get()
+        if letter == word[y]:
+            userLetters[y] = 1
+            gridEntries[row][y].config(disabledbackground = "#008000")
+            for keybtn in keyboardEntries:
+                if keybtn["text"] == letter:
+                    keybtn.config(bg = "#008000")
+            
+    for y in range(5):
+        letter = gridEntries[row][y].get()
+        if letter != word[y] and letter in word:
+            for i in range(5):
+                if word[i] == letter and userLetters[i] == 0:
+                    userLetters[i] = 1  
+                    gridEntries[row][y].config(disabledbackground = "#FFBF00")
+                    for keybtn in keyboardEntries:
+                        if keybtn["text"] == letter:
+                            keybtn.config(bg = "#FFBF00")
+                            
+    for y in range(5):
+        letter = gridEntries[row][y].get()
+        if letter not in word:
+            for keybtn in keyboardEntries:
+                if keybtn["text"] == letter:
+                    keybtn.config(bg = "#5A5A5A")
+            
+    for y in range(5):
+        userWord += gridEntries[row][y].get()
+        
+    if (userWord == word):
+        end_game("win", window)
+    
+def end_game(status, window):
+    global word
+    
+    if status == "win":
+        messagebox.showinfo("Win", "Congratulations you have won")
+    
+    if status == "lose":
+        messagebox.showinfo("Lose", "The word was " + word)
+    
+    window.quit()
+    sys.exit()
             
 def upper_case(event, x, y):
     gridEntries[x][y].delete(0, END)
@@ -99,7 +276,7 @@ def upper_case(event, x, y):
 def create_window():
     window = Tk()
     window.title("Wordle")
-    window['bg'] = "#3b3b3b"
+    window['bg'] = "#222222"
     
     windowWidth = 600
     windowHeight = 800
@@ -118,6 +295,8 @@ def main():
     window = create_window()
     
     create_letterbox(window)
+    
+    create_buttons(window)
     
     generate_word()
     
